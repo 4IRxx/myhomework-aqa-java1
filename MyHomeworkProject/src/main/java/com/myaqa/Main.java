5d0c286fb2cf481ec74cd31a302910f2bb330c3b
Anton <simanchuk1997@gmail.com>
2023-12-14 16:17:42 +0300

    create Maven project

---
+++ MyHomeworkProject/src/main/java/com/myaqa/Main.java
@@ -0,0 +1,9 @@
+package com.myaqa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!"); //основной класс в ветке main, источник ветвлений
+    }
+}
